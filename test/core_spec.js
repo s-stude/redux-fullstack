f0100ca472333bbbce9412f45900b453c3d2ff9d
Akim Khalilov <Akim.Khalilov@gmail.com>
2016-04-28 21:31:08 +0300

    First logic setEntries

---
+++ test/core_spec.js
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import {List,Map} from 'immutable';
+
+import {setEntries} from '../src/core';
+
+describe('application logic', ()=> {
+
+    describe('setEntries', ()=> {
+
+        it('adds the entries to the state', ()=> {
+
+            const state = Map();
+            const entries = ['Trainspotting', '28 days later'];
+            const nextState = setEntries(state, entries);
+
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 days later')
+            }));
+
+
+        });
+
+    });
+
+});
