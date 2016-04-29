47f39e40aa8ea880858330dd0fd777412443a71c
Akim Khalilov <Akim.Khalilov@gmail.com>
2016-04-29 23:11:23 +0300

    Introducing Actions and Reducers

--- src/core.js
+++ src/core.js
@@ -36,3 +36,5 @@ export function next(state) {
 export function vote(state, entry) {
     return state.updateIn(['vote', 'tally', entry], 0, tally => tally + 1);
 }
+
+export const INITIAL_STATE = Map();
