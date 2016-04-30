aaa0cb23f6f210e25c8358318f3b1ca29ddef05e
Akim Khalilov <Akim.Khalilov@gmail.com>
2016-04-30 16:48:14 +0300

    Reducer composition

--- test/core_spec.js
+++ test/core_spec.js
@@ -114,24 +114,17 @@ describe('application logic', ()=> {
         it('creates a tally for the voted entry', ()=> {
 
             const state = Map({
-                vote   : Map({
-                    pair: List.of('Trainspotting', '28 days later')
-                }),
-                entries: List()
+                pair: List.of('Trainspotting', '28 days later')
             });
 
             const nextState = vote(state, 'Trainspotting');
 
             expect(nextState).to.equal(Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 days later'),
+                pair: List.of('Trainspotting', '28 days later'),
 
-                    tally: Map({
-                        'Trainspotting': 1
-                    })
-                }),
-
-                entries: List()
+                tally: Map({
+                    'Trainspotting': 1
+                })
             }));
 
         });
@@ -139,28 +132,21 @@ describe('application logic', ()=> {
         it('adds to existing tally for the voted entry', ()=> {
 
             const state = Map({
-                vote   : Map({
-                    pair : List.of('Trainspotting', '28 days later'),
-                    tally: Map({
-                        'Trainspotting': 3,
-                        '28 days later': 2
-                    })
-                }),
-                entries: List()
+                pair : List.of('Trainspotting', '28 days later'),
+                tally: Map({
+                    'Trainspotting': 3,
+                    '28 days later': 2
+                })
             });
 
             const nextState = vote(state, 'Trainspotting');
 
             expect(nextState).to.equal(Map({
-                vote: Map({
-                    pair : List.of('Trainspotting', '28 days later'),
-                    tally: Map({
-                        'Trainspotting': 4,
-                        '28 days later': 2
-                    })
-                }),
-
-                entries: List()
+                pair : List.of('Trainspotting', '28 days later'),
+                tally: Map({
+                    'Trainspotting': 4,
+                    '28 days later': 2
+                })
             }));
 
         });
