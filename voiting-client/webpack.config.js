ad8f6b812d2027e53aa5dc5acb3178403c06ff65
Akim Khalilov <Akim.Khalilov@gmail.com>
2016-04-30 18:16:00 +0300

    react and react-hot-loader

--- voiting-client/webpack.config.js
+++ voiting-client/webpack.config.js
@@ -1,11 +1,17 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
 
     module: {
         loaders: [{
             test   : /\.jsx?$/,
             exclude: /node_modules/,
-            loader : 'babel'
+            loader : 'react-hot!babel'
         }]
     },
 
@@ -20,6 +26,11 @@ module.exports = {
     },
 
     devServer: {
-        contentBase: './dist'
-    }
+        contentBase: './dist',
+        hot        : true
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
